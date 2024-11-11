2adf2ba07a40f9ff9de3aa3bd1e3d126effbd856
nthanh12 <nguyenkimthanh25122002@gmail.com>
2024-11-11 22:43:03 +0700

    upcode 11-11: added unit test AddProductAsync method in ProductService

--- MShop.Infrastructure/obj/Debug/net8.0/MShop.Infrastructure.AssemblyInfo.cs
+++ MShop.Infrastructure/obj/Debug/net8.0/MShop.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MShop.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bdfde2f97abf67f2187c8e8b0ee11a3f3c11e04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b859975edbc3f4b32a999c2992385531a99a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("MShop.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MShop.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
